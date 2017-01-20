08881d88a9785ea519870eb6487773b98561172d
Sören Oldag <soeren_oldag@freenet.de>
2017-01-20 18:33:51 +0100

    Fix package description.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pwmled',
     version='1.0.0',
-    description='Control LimitlessLED products.',
+    description='Control pwm leds on a Raspberry Pi.',
     url='https://github.com/soldag/python-pwmled/',
     license='MIT',
     author='soldag',
