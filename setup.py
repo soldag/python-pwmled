f4cc46f7fd8e8a888c9b9a5b214339c69928024d
Sören Oldag <soeren_oldag@freenet.de>
2017-02-13 01:31:20 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='pwmled',
     version='1.0.0',
-    description='Control pwm leds on a Raspberry Pi.',
+    description='Control LEDs connected to a micro controller using pwm.',
     url='https://github.com/soldag/python-pwmled/',
     license='MIT',
     author='soldag',
     author_email='soren.oldag@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'pigpio',
-        'adafruit-pca9685'
+        'pigpio==1.35',
+        'Adafruit-PCA9685==1.0.1'
     ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
