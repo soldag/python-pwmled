4e41c9e06c07e26993789859985344b02c62bf6a
Sören Oldag <soeren_oldag@freenet.de>
2017-02-14 14:23:38 +0100

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pwmled',
-    version='1.0.0',
+    version='1.1.0',
     description='Control LEDs connected to a micro controller using pwm.',
     url='https://github.com/soldag/python-pwmled/',
     license='MIT',
